b33946ef5397a851026ec0c0e2225541d6c86754
enwalker33 <44973965+enwalker33@users.noreply.github.com>
2020-05-05 04:42:35 +0000

    MyFifthProject - Style and Layout

---
+++ MyFifthProject/App copy 6.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }}
+    >
+      <View
+        style={{ width: 50, height: 50, backgroundColor: 'red' }}
+      ></View>
+      <View
+        style={{ width: 50, height: 50, backgroundColor: 'yellow' }}
+      ></View>
+      <View style={{ width: 50, height: 50, backgroundColor: 'green' }}></View>
+    </View>
+  );
+}
